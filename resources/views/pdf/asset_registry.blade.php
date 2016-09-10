c78aef424217edbecfc6c19eebe553868ca0e07f
Daryl Dangan <dangan.daryl5@gmail.com>
2016-09-10 20:43:28 +0800

    Changed some style in PDF for testing

--- resources/views/pdf/asset_registry.blade.php
+++ resources/views/pdf/asset_registry.blade.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
   	<head>
+    <style type="text/css">
+     body {
+          font-family: "Open Sans", "Arial", "Calibri", sans-serif;
+          font-size: 12px;
+        }
+        .header p{
+          margin: 5px;
+        }
+        th {
+          background: #eee;
+        }
+        table, th, td {
+          border: 1px solid #000;
+          padding: 5px;
+        }
+    </style>
   	</head>
   	<body>
-       	<div align="center">
-		     <p><strong>Somerset Homeowners Association</strong></p>
-		     <p>Asset Registry</p>
-		     <p>{{date('F')}}, {{date('Y')}} </p>
+       	<div class="header" align="center">
+		     <th><strong>Somerset Homeowners Association</strong></th>
+		     <th>Asset Registry</th>
+		     <th>{{date('F')}}, {{date('Y')}} </th>
 		</div>
 		<hr/>
 		<table style="width:100%; border-collapse: collapse; text-align:center;">
@@ -21,6 +37,9 @@
                 </tr>
           	</thead>
           	<tbody>
+              <tr>
+                <td></td>
+              </tr>
           		@if(empty($assetItemList))
     				<tr><td colspan="5" align="center"><em><strong> No Records Found </strong></em></td></tr>
     			@else
