9447fcbe5262ce1fb8d8ddf2aad507bdee2fa207
Kristopher Veraces <kristopher.n.veraces@gmail.com>
2016-09-15 02:52:12 +0800

    Added validation for telephone number

--- app/Http/Requests/vendor/VendorRequest.php
+++ app/Http/Requests/vendor/VendorRequest.php
@@ -43,7 +43,7 @@ class VendorRequest extends Request
                 return ['vendor_name' => 'required|min:3|max:255|unique:vendors,vendor_name,' . $vendor->id,
                         'vendor_description' => 'required|min:3|max:255',
                         'vendor_contact_person' => 'required|min:3|max:255',
-                        'vendor_telephone_no' => 'numeric|digits_between:7,11',
+                        'vendor_telephone_no' => 'numeric|digits_between:7,11|min:1',
                         'vendor_mobile_no' => 'required|numeric|digits_between:11,13|min:1',
                         'vendor_email_address' => 'required|email|max:255|unique:vendors,vendor_email_address,' . $vendor->id,];
             }
