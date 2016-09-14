48c6f2a72cea45ac393e47600fbfb01bd11a1986
Kristopher Veraces <kristopher.n.veraces@gmail.com>
2016-09-15 03:02:51 +0800

    Added validation on update of homeowner member

--- app/Http/Requests/homeownermember/HomeOwnerMemberRequest.php
+++ app/Http/Requests/homeownermember/HomeOwnerMemberRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\homeownermember;
 
 use App\Http\Requests\Request;
+use App\HomeOwnerMemberModel;
 
 class HomeOwnerMemberRequest extends Request
 {
@@ -23,18 +24,44 @@ class HomeOwnerMemberRequest extends Request
      */
     public function rules()
     {
-        return [
-            'companion_gender' => 'required',
-            'first_name' => 'required|min:2|max:225',
-            'middle_name' => 'required|min:2|max:225',
-            'last_name' => 'required|min:2|max:225',
-            'companion_email_address' => 'email|max:255|unique:home_owner_member_information',
-            'companion_mobile_no' => 'numeric|digits_between:11,13|min:1',
-            'companion_occupation' => 'min:3|max:225',
-            'companion_office_tel_no' => 'numeric|digits_between:7,11|min:1',
-            'companion_date_of_birth' => 'required|before:today',
-            'home_owner_id'=>'required',
-            'relationship'=>'required|min:3|max:225',
-        ];
+        $homeownerMember = HomeOwnerMemberModel::find($this->homeownermembers);
+        switch($this->method())
+        {
+            case 'GET': break;
+            case 'DELETE': break;
+            //for insert
+            case 'POST':{
+                return ['companion_gender' => 'required',
+                        'first_name' => 'required|min:2|max:225',
+                        'middle_name' => 'required|min:2|max:225',
+                        'last_name' => 'required|min:2|max:225',
+                        'companion_email_address' => 'email|max:255|unique:home_owner_member_information',
+                        'companion_mobile_no' => 'numeric|digits_between:11,13|min:1',
+                        'companion_occupation' => 'min:3|max:225',
+                        'companion_office_tel_no' => 'numeric|digits_between:7,11|min:1',
+                        'companion_date_of_birth' => 'required|before:today',
+                        'home_owner_id'=>'required',
+                        'relationship'=>'required|min:3|max:225',
+                    ];
+            }
+            //for update
+            case 'PATCH':{  
+                return ['companion_gender' => 'required',
+                        'first_name' => 'required|min:2|max:225',
+                        'middle_name' => 'required|min:2|max:225',
+                        'last_name' => 'required|min:2|max:225',
+                        'companion_email_address' => 'email|max:255|unique:home_owner_member_information,companion_email_address,'. $homeownerMember->id,
+                        'companion_mobile_no' => 'numeric|digits_between:11,13|min:1',
+                        'companion_occupation' => 'min:3|max:225',
+                        'companion_office_tel_no' => 'numeric|digits_between:7,11|min:1',
+                        'companion_date_of_birth' => 'required|before:today',
+                        'home_owner_id'=>'required',
+                        'relationship'=>'required|min:3|max:225',
+                        ];
+            }
+            //default
+            default: break;
+        }
+        
     }
 }
