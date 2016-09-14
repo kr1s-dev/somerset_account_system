7c740846e81671e9cc4abd6e10221e050ff12e86
Kristopher Veraces <kristopher.n.veraces@gmail.com>
2016-09-15 03:11:25 +0800

    Change label to New Password Confirmation

--- resources/views/users/change_password.blade.php
+++ resources/views/users/change_password.blade.php
@@ -38,7 +38,7 @@
                      </div>
                   </div>
                   <div class="form-group">
-                     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Old Password <span class="required">*</span>
+                     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">New Password Confirmation<span class="required">*</span>
                      </label>
                      <div class="col-md-3 col-sm-6 col-xs-12">
                         <input type="password" name="new_password_confirmation" id="last_name" required="required" class="form-control col-md-7 col-xs-12">
