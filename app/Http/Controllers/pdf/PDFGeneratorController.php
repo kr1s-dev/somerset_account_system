63e6c307b95f916297da9994eb57fb8de6ec82d5
Kristopher <kristopher.n.veraces@gmail.com>
2016-09-19 00:42:44 +0800

    Fixed Bug in PDF

--- app/Http/Controllers/pdf/PDFGeneratorController.php
+++ app/Http/Controllers/pdf/PDFGeneratorController.php
@@ -189,7 +189,7 @@ class PDFGeneratorController extends Controller
 
         $aTitleItemsList = $this->getJournalEntryRecordsWithFilter(null,$monthFilter,$yearFilter);
         //print_r($aTitleItemsList);
-        $eBalanceSheetItemsList = $this->getItemsAmountList($aTitleItemsList,null);
+        $eBalanceSheetItemsList = $this->getCustomItemsAmountList($aTitleItemsList,null);
 
 
         foreach ($accountTitleGroupList as $accountTitleGroup) {
@@ -199,8 +199,6 @@ class PDFGeneratorController extends Controller
         }
         //print_r($eBalanceSheetItemsList);
         foreach ($accountTitlesList as $accountTitle) {
-            
-
             if (array_key_exists($accountTitle->account_sub_group_name,$eBalanceSheetItemsList)) {
                 if(array_key_exists($accountTitle->group->account_group_name,$fBalanceSheetItemsList)){
                     $tArray = $fBalanceSheetItemsList[$accountTitle->group->account_group_name];
@@ -212,6 +210,8 @@ class PDFGeneratorController extends Controller
             }
         }
 
+        //print_r($fBalanceSheetItemsList);
+        
         foreach (array_keys($fBalanceSheetItemsList) as $key) {
             if(strpos($key, 'Assets')){
                 $totalAssets+= ($this->getTotalSum($fBalanceSheetItemsList[$key]));
@@ -233,6 +233,42 @@ class PDFGeneratorController extends Controller
                                         'totalLiability'));
     }
 
+    public function getCustomItemsAmountList($arrayToProcessList,$typeOfData){
+        $data = array();
+        if($typeOfData == 'Equity'){
+            $accountGroup =  AccountGroupModel::where('account_group_name', 'like', '%'.$typeOfData.'%')
+                                                ->get();
+            foreach ($accountGroup as $accountGrp) {
+                foreach ($accountGrp->accountTitles as $accountTitle) {
+                    $data[$accountTitle->account_sub_group_name] = 0;
+                }
+            }
+        }else if(is_null($typeOfData)){
+            $accountGroup =  $this->getAccountGroups(null);
+            foreach ($accountGroup as $accountGrp) {
+                foreach ($accountGrp->accountTitles as $accountTitle) {
+                    $data[$accountTitle->account_sub_group_name] = $accountTitle->opening_balance;
+                }
+            }
+        }
+
+        if(!empty($arrayToProcessList)){
+            foreach ($arrayToProcessList as $arrayToProcess) {
+                $typeOfData = $arrayToProcess->credit_title_id == NULL ? $arrayToProcess->debit->group->account_group_name : $arrayToProcess->credit->group->account_group_name;
+                $amount = ($arrayToProcess->debit_amount - $arrayToProcess->credit_amount);
+                $accountTitle = $arrayToProcess->credit_title_id == NULL ? $arrayToProcess->debit->account_sub_group_name : $arrayToProcess->credit->account_sub_group_name;
+
+                if(array_key_exists($accountTitle,$data)){
+                    $data[$accountTitle] += (strpos($typeOfData, 'Revenues') !== false || strpos($typeOfData, 'Equity') | strpos($typeOfData, 'Liabilities') ? 
+                                                ($amount * -1)  : $amount);
+                }else{
+                    $data[$accountTitle] = $typeOfData == 'Revenues' ? ($amount * -1)  : $amount;
+                }
+            }
+        }
+        return $data;
+    }
+
 
     public function generateSubsidiaryLedger($monthFilter,$yearFilter,$type){
         $yearFilter = $yearFilter==NULL?date('Y'):date($yearFilter);
@@ -252,8 +288,10 @@ class PDFGeneratorController extends Controller
         $itemsList = $query->get();
         
         foreach ($itemsList as $item) {
-            $payeeName = $type=='homeowner' ? ($item->invoice->homeOwner->first_name . ' ' . $item->invoice->homeOwner->middle_name . ' ' .$item->invoice->homeOwner->last_name) : 
-                                $item->vendor_id==null?$item->paid_to:$item->vendor->vendor_name;
+            $payeeName = $type=='homeowner' ? $item->invoice->homeOwner->first_name . ' ' . 
+                                $item->invoice->homeOwner->middle_name . ' ' .
+                                $item->invoice->homeOwner->last_name : 
+                                ($item->paid_to!=''?$item->paid_to:$item->vendor->vendor_name);
 
             
             foreach (($type=='homeowner'? $item->invoice->invoiceItems : $item->expenseItems)  as $val) {
